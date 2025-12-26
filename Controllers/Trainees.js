6cb5aede2dd5cccfb47e663f5a3a9af92a0f63e4
CHARAN2066 <147472977+CHARAN2066@users.noreply.github.com>
2025-12-26 12:11:06 +0530

    sid

--- Controllers/Trainees.js
+++ Controllers/Trainees.js
@@ -11,5 +11,15 @@ const get_details=async(req,res)=>{
     const data=await Trainees.findOne({name:id})
     res.json(data);
 }
-export {addTrainee,get_details};
+
+const update_details=async(req,res)=>{
+    const name=req.params.name;
+    const data=req.body.name;
+    await Trainees.updateOne(
+        {name:name},
+        {$set:{name:data}}
+    );
+    res.send("data Updated")
+}
+export {addTrainee,get_details,update_details};
 
--- Routers/TraineeRouter.js
+++ Routers/TraineeRouter.js
@@ -1,9 +1,10 @@
-import {addTrainee,get_details} from '../Controllers/Trainees.js';
+import {addTrainee,get_details,update_details} from '../Controllers/Trainees.js';
 import express from 'express';
 
 const router=express.Router()
 
 router.post('/add_data',addTrainee);
 router.get('/get_data/:id',get_details);
+router.put('/update_details/:name',update_details);
 
 export default router;
